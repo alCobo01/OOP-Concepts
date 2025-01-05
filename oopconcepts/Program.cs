4bd75b4ed489cf6144825cd553096c85cd54c049
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-01-05 16:28:46 +0100

    Closes #1 and #2

--- oopconcepts/Program.cs
+++ oopconcepts/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace oopconcepts
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Door metalicDoor = new Door("Metal", "Gray");
+            SlidingDoor woodenSlidingDoor = new SlidingDoor("Wood", "Brown", true);
+
+            Console.WriteLine($"Is door 1 made of wood? {HelperMethods.IsMadeOfWood(metalicDoor)}");
+            Console.WriteLine($"Is door 2 made of wood? {HelperMethods.IsMadeOfWood(woodenSlidingDoor)}");
+        }
+    }
+}
